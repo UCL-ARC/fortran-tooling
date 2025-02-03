f316c38dc0cc39a664b5e61fd8f23e368e25a60c
Connor Aird <c.aird@ucl.ac.uk>
2025-02-03 17:53:31 +0000

    Add test for poisson::inp

--- testing/garden/main.f90
+++ testing/garden/main.f90
@@ -1,6 +1,7 @@
 program garden_main
     use garden, only : test_item_t, test_that, run_tests
     use garden_mesh_generator, only : test_mesh_generator
+    use garden_poisson, only : test_poisson
     implicit none
 
     if (.not.run()) stop 1
@@ -10,9 +11,10 @@ contains
         logical :: passed
 
         type(test_item_t) :: tests
-        type(test_item_t) :: individual_tests(1)
+        type(test_item_t) :: individual_tests(2)
 
         individual_tests(1) = test_mesh_generator()
+        individual_tests(2) = test_poisson()
         
         tests = test_that(individual_tests)
 
