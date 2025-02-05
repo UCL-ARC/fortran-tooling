ebfab6319b22f99966715d40050faa1f87a7dc6b
Connor Aird <c.aird@ucl.ac.uk>
2025-02-05 11:30:08 +0000

    Add test for poisson using Given, When and Then structure

--- testing/veggies/main.f90
+++ testing/veggies/main.f90
@@ -2,6 +2,7 @@ program veggies_main
     use veggies, only : test_item_t, test_that, run_tests
     use veggies_mesh_generator, only : test_mesh_generator
     use veggies_poisson, only : test_poisson
+    use veggies_poisson_given_when_then, only : test_poisson_given_when_then
     implicit none
 
     if (.not.run()) stop 1
@@ -11,10 +12,11 @@ contains
         logical :: passed
 
         type(test_item_t) :: tests
-        type(test_item_t) :: individual_tests(2)
+        type(test_item_t) :: individual_tests(3)
 
         individual_tests(1) = test_mesh_generator()
         individual_tests(2) = test_poisson()
+        individual_tests(3) = test_poisson_given_when_then()
         
         tests = test_that(individual_tests)
 
