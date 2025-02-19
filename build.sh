e6e05344e90ea04948e3e65e94fe46b0bf7a59ef
Connor Aird <c.aird@ucl.ac.uk>
2025-02-19 17:52:48 +0000

    Add ability to run tests to the build.sh script

--- build.sh
+++ build.sh
@@ -6,14 +6,16 @@ PFUNIT_INSTALLED_PATH="" # Path to pFUnit/installed directory
 clean_build=false
 build_cmake=false
 build_fpm=false
+run_tests=false
 
 help() {
   echo "Usage:"
   echo "    -h  Display this help message."
-  echo "    -r  Remove all build artifacts."
-  echo "    -c  Build via cmake."
+  echo "    -c  Clean all build artifacts."
+  echo "    -m  Build via cmake."
   echo "    -f  Build via fpm."
   echo "    -b  Build with all build tools."
+  echo "    -t  Run tests."
   exit 0
 }
 
@@ -25,16 +27,17 @@ then
 fi
 
 # parse input arguments
-while getopts "hrcfb" opt
+while getopts "hcmfbt" opt
 do
   case ${opt} in
     h  ) help;;
-    r  ) clean_build=true;;
-    c  ) build_cmake=true;;
+    c  ) clean_build=true;;
+    m  ) build_cmake=true;;
     f  ) build_fpm=true;;
     b  ) 
         build_cmake=true
         build_fpm=true;;
+    t  ) run_tests=true;;
     \? ) echo "Invalid option: $OPTARG" >&2; exit 1;;
   esac
 done
@@ -78,4 +81,27 @@ then
     fi
     echo "Building using fpm"
     fpm build
+fi
+
+if [ "$run_tests" == "true" ]
+then
+    tests_run=false
+    if [ -d "build-cmake" ]; then
+        echo "Running cmake tests"
+        tests_run=true
+        pushd build-cmake
+        ctest
+        popd
+    fi
+
+    if [ -d "build" ]; then
+        echo "Running fpm tests"
+        tests_run=true
+        fpm test
+    fi
+
+    if [ "$tests_run" == "false" ]
+    then
+        echo "No tests found. Must build first."
+    fi
 fi
