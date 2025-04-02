a0674446ff8c17cb04767b70c893e34653b83db1
Connor Aird <c.aird@ucl.ac.uk>
2025-04-02 16:07:11 +0100

    Document pFUnit tests and fix failing test

--- src/mesh_generator/mesh_generator.f90
+++ src/mesh_generator/mesh_generator.f90
@@ -1,4 +1,5 @@
 !> This module contians proceducers for generating a basic square 2D triangular-mesh.
+!>
 !> |mesh_geometry_description|
 module mesh_generator
     use, intrinsic :: iso_fortran_env, only : int64, real64
